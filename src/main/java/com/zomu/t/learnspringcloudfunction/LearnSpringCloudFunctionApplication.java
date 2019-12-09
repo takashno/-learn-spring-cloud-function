ab77b46084c4ce72f04eb69ea414fcaddae7480c
unknown <takashno@alpha.co.jp>
2019-12-09 12:59:21 +0900

    init

---
+++ src/main/java/com/zomu/t/learnspringcloudfunction/LearnSpringCloudFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.zomu.t.learnspringcloudfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringCloudFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringCloudFunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zomu/t/learnspringcloudfunction/LearnSpringCloudFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zomu.t.learnspringcloudfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCloudFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
